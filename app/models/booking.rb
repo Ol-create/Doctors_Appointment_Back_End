4a46f47c0cb0547e5418602084ccf06b556eaabe
Oluyemi Paul <oluola96@gmail.com>
2023-01-27 19:35:06 +0000

    resolve linters err

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-    belongs_to :user
-    belongs_to :doctor
-    validates :time, :date, presence: true
-  end
+  belongs_to :user
+  belongs_to :doctor
+  validates :time, :date, presence: true
+end
