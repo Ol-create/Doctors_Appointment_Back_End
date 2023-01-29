2fcdecb58672fadd1020389e8a035f94db052871
mullenlucas <mullenlucas@gmail.com>
2023-01-29 17:25:55 -0300

    fix rubocop offenses

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Booking, type: :model do
   describe 'validation tests' do
     subject do
       @usero = User.new(name: 'Nuk Tashino', email: 'nuk@tashino.com', password: '123456',
-                            password_confirmation: '123456')
+                        password_confirmation: '123456')
       @doctoro = Doctor.new(user_id: @usero.id, name: 'Rick', location: 'Ireland',
-                                specialization: 'Pathologist', price: 400)
+                            specialization: 'Pathologist', price: 400)
       Booking.new(user_id: @usero.id, doctor_id: @doctoro.id, booking_date: '27-01-2023')
     end
 
