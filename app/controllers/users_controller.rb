dfb196c390ea73a4232a882794951d6a3a6952ab
mullenlucas <mullenlucas@gmail.com>
2023-01-30 12:02:07 -0300

    add session controller for managing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,14 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:id] = @user.id
+      payload = { user_id: @user.id }
+      token = encode_token(payload)
+      render json: { user: @user, jwt: token }
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { status: 'error', errors: @user.errors.full_messages }
     end
   end
 
