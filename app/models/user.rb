213f2ab4d32be4614459c6cad65614a9d7c6adc5
mullenlucas <mullenlucas@gmail.com>
2023-01-30 12:08:59 -0300

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :doctors
   has_many :bookings
-  validates :name, :email, :password, presence: true
+  validates :name, :email, :password, :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
+  validates :name, uniqueness: { case_sensitive: false }
 end
